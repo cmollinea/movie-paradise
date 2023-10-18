3e63c001461539dea98e0fbbcf3855d5ae485448
Carlos Yoan Mollinea Perez <mollinea99@gmail.com>
2023-10-18 13:25:15 -0400

    feat: add response types and api endpoints

---
+++ src/app/constants/ApiEndpoints.ts
@@ -0,0 +1,27 @@
+export const POPULAR_MOVIES_ENDPOINT =
+  'https://api.themoviedb.org/3/movie/popular';
+//Get a list of movies ordered by popularity.
+
+export const TRENDING_MOVIES_ENDPOINT =
+  'https://api.themoviedb.org/3/trending/movie/';
+//Get the trending movies on TMDB.
+
+export const TOP_RATED_MOVIES_ENDPOINT =
+  'https://api.themoviedb.org/3/movie/top_rated';
+//Get a list of movies ordered by rating.
+
+export const UPCOMING_MOVIES_API =
+  'https://api.themoviedb.org/3/movie/upcoming';
+//Get a list of movies that are being released soon.
+
+export const POPULAR_TV_SHOWS_ENDPOINT =
+  'https://api.themoviedb.org/3/tv/popular';
+//Get a list of TV shows ordered by popularity.
+
+export const AIRING_TODAY_TV_SHOWS_ENDPOINT =
+  'https://api.themoviedb.org/3/tv/airing_today';
+//Get a list of TV shows airing today.
+
+export const ON_THE_AIR_TV_SHOWS_ENDPOINT =
+  'https://api.themoviedb.org/3/tv/on_the_air';
+//Get a list of TV shows that air in the next 7 days.
