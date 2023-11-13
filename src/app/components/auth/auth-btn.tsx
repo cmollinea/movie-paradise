65d979d7659ecd0f040d55b251378d70a4f5d207
Carlos Yoan Mollinea Perez <mollinea99@gmail.com>
2023-11-12 23:33:25 -0500

    fix: add auth btn

---
+++ src/app/components/auth/auth-btn.tsx
@@ -0,0 +1,11 @@
+'use client';
+import { Button } from '@nextui-org/react';
+
+function AuthenticationButton() {
+  return (
+    <Button variant='solid' color='primary'>
+      Sign In
+    </Button>
+  );
+}
+export default AuthenticationButton;
